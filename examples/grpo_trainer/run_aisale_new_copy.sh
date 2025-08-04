988e0e7dc1f4a1c860b6ebd2e430bb0ce060507e
aiqiuyuan <80582035+aiqiuyuan@users.noreply.github.com>
2025-08-04 18:40:34 +0800

    Update run_aisale_new_copy.sh

--- examples/grpo_trainer/run_aisale_new_copy.sh
+++ examples/grpo_trainer/run_aisale_new_copy.sh
@@ -6,12 +6,14 @@ export HYDRA_FULL_ERROR=1
 export WANDB_BASE_URL=https://api.bandw.top
 wandb login edfcef4f9801daf389c28a3b7bc6b75073fdccfb
 
-train_files="/mnt/bn/motor-nlp-team/users/aiqiuyuan/verl_debug/data/0515/data_ai_baseline/train.parquet"
-val_files="/mnt/bn/motor-nlp-team/users/aiqiuyuan/verl_debug/data/0515/data_ai_baseline/val.parquet"
-test_files="/mnt/bn/motor-nlp-team/users/aiqiuyuan/verl_debug/data/0515/data_ai_baseline/test.parquet"
-score_log_path="/mnt/bn/motor-nlp-team/users/aiqiuyuan/verl_debug/score_records/score_records_0708_ai_baseline_new_reward_4.jsonl"
-debug_log_path="/mnt/bn/motor-nlp-team/users/aiqiuyuan/verl_debug/debug_records/debug_records_0708_ai_baseline_new_reward_4.jsonl"
-tool_log_path="/mnt/bn/motor-nlp-team/users/aiqiuyuan/verl_debug/tool_records/tool_records_0708_ai_baseline_new_reward_4.jsonl"
+train_files=""
+val_files=""
+test_files=""
+score_log_path=""
+debug_log_path=""
+tool_log_path=""
+model_path=""
+reward_path=""
 
 
 python3 -m verl.trainer.main_ppo \
@@ -28,7 +30,7 @@ python3 -m verl.trainer.main_ppo \
     ++actor_rollout_ref.rollout.max_num_batched_tokens=14000 \
     data.filter_overlong_prompts=True \
     data.truncation='error' \
-    actor_rollout_ref.model.path=/mnt/bn/motor-nlp-team/models/LLM/base_models/Qwen2.5-7B-Instruct \
+    actor_rollout_ref.model.path="$model_path" \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.ppo_mini_batch_size=4 \
@@ -47,17 +49,15 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.n=5 \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=1 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
-    custom_reward_function.path=/mnt/bn/motor-nlp-team/users/aiqiuyuan/verl_debug/verl/utils/reward_score/ai_sale_0722_baseline.py \
+    custom_reward_function.path="$reward_path" \
     custom_reward_function.name=compute_score \
     algorithm.use_kl_in_reward=False \
     trainer.critic_warmup=0 \
     trainer.logger=['console','wandb'] \
     trainer.project_name='aisale_0708' \
-    trainer.experiment_name='ai_baseline_new_reward_4' \
+    trainer.experiment_name='ToolDNA' \
     trainer.n_gpus_per_node=8 \
     trainer.nnodes=1 \
     trainer.save_freq=100 \
     trainer.test_freq=5 \
-    trainer.resume_mode=resume_path \
-    trainer.resume_from_path="/mnt/bn/motor-nlp-team/users/aiqiuyuan/verl_debug/sft/global_step_1101" \
     trainer.total_epochs=6 $@
